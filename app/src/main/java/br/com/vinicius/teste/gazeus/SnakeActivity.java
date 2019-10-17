3ac92148ed9ff84e55224f3659b8838a969e3dfd
Vinicius Fortino <vinicius.fortino@gmail.com>
2019-10-17 13:53:42 -0300

    initial commit

---
+++ app/src/main/java/br/com/vinicius/teste/gazeus/SnakeActivity.java
@@ -0,0 +1,12 @@
+package br.com.vinicius.teste.gazeus;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SnakeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
